6010485d633c7f2a25653bddad07892de3f26944
qingsheng.sun <somereason@gmail.com>
2022-02-08 16:51:14 +0800

    init

---
+++ config.js
@@ -0,0 +1,11 @@
+exports.token=[
+    { name: "BTC", symbol: "btcusdt" },
+    { name: "ETH", symbol: "ethusdt" },
+    { name: "ADA", symbol: "adausdt" }
+]
+
+exports.stock=[
+    { name: "中信证券", symbol: "sh600030" },
+    { name: "芯片ETF", symbol: "sz159995" },
+    { name: "三聚环保", symbol: "sz300072" }
+]
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const axios = require('axios')
+const config = require('./config')
+const app = express()
+const port = 8088
+
+async function getTokenPrice() {
+    const tokens = config.token
+    let tokenRequest = []
+    tokens.forEach(a => {
+        tokenRequest.push(axios.get("https://api.huobi.pro/market/history/kline?period=1min&size=1&symbol=" + a.symbol))
+    })
+    let tokenResponse = await Promise.all(tokenRequest);
+    let tokenResult = [];
+    for (let i = 0; i < tokens.length; i++) {
+        tokenResult.push({ name: tokens[i].name, value: tokenResponse[i].data.data[0].close });
+    }
+    return tokenResult;
+}
+async function getDollarPrice() {
+    let resp = await axios.get("https://api.exchangerate-api.com/v4/latest/USD");
+    let result = [];
+    result.push({ name: "美元对人民币", value: resp.data.rates.CNY });
+    return result;
+}
+async function getStockPrice() {
+    const stocks2Query = config.stock;
+
+    let queryString = [];
+    stocks2Query.forEach(a => { queryString.push("s_" + a.symbol) })
+    let reqString = `http://qt.gtimg.cn/r=0.8409869808238q=${queryString.join(",")}`;
+    // v_s_sz000559="51~万向钱潮~000559~5.81~0.07~1.22~110475~6384~~191.95~GP-A";
+    let resp = await axios.get(reqString);
+    const stocks = resp.data.split(";");
+    let result = [];
+    for (let i = 0; i < stocks.length; i++) {
+        if (i >= stocks2Query.length)
+            continue
+        let info = stocks[i].split("~");
+        result.push({ name: stocks2Query[i].name, price: info[3], rate: info[5] })
+    }
+
+    return result;
+}
+app.get('/', async (req, res) => {
+    let result = []
+    let [token, dollar, stock] = await Promise.all([getTokenPrice(), getDollarPrice(), getStockPrice()])
+    result.push(...token);
+    result.push(...dollar);
+    result.push(...stock);
+
+    res.send(result)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+//http://qt.gtimg.cn/r=0.8409869808238q=s_sz000559,s_sh600895,s_sz002048,s_sz002085,s_sz002126,s_sz002284,s_sz002434,s_sz002472,s_sz002488
